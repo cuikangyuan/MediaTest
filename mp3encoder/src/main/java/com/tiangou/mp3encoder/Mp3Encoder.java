d0924778b0f68c283a00bf7094494fea62045731
cuikangyuan <cuikangyuan@51tiangou.com>
2019-09-26 18:04:17 +0800

    add jni test code

---
+++ mp3encoder/src/main/java/com/tiangou/mp3encoder/Mp3Encoder.java
@@ -0,0 +1,4 @@
+package com.tiangou.mp3encoder;
+
+public class Mp3Encoder {
+}
---
+++ mp3encoder/src/main/jni/Mp3Encoder.cpp
@@ -0,0 +1,11 @@
+//
+// Created by tiangou on 2019-09-26.
+//
+#include <com_tiangou_mp3encoder_Mp3Encoder.h>
+
+
+JNIEXPORT void JNICALL Java_com_tiangou_mp3encoder_Mp3Encoder_encode
+  (JNIEnv *, jobject) {
+
+  LOGI("encode encode");
+}
